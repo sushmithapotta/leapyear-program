936bc0934b2d67b9fc1b21cb553219bbad7aa3ff
sushmithapotta <pottasushmitha9@gmail.com>
2025-01-30 22:15:32 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,12 @@
+public class Leapyear {
+    public static void main(String[] args) {
+        int a=2005;
+        if(a%4==0){
+            System.out.println("leap year");
+
+        }else{
+            System.out.println("not a leap year");
+        }
+    }
+}
+
